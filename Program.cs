f53c7794dab80f022de6e2e167edf2c5daaf06ca
Ayşegül Albayrak <79749647+Aysegulalbayrak@users.noreply.github.com>
2021-03-14 18:20:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,76 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace odev
+namespace DersBasariHesaplama
 {
     class Program
     {
         static void Main(string[] args)
         {
+        /* Vize, final ve odev notlarını klavyeden alalım
+         * Vizenin ->%30
+         * Finalin ->%40
+         * Odevin -> %30  
+         * ortalamaya etkilemektedir.
+         *
+         * Ortalama 40 dan düşükse dersten başarısız oldun seneye tekrar al.
+         * Ortalama 40 ve 60 arasında Büte girebilirsiniz
+         * Ortalama 60 ve üzeri ise dersten başarılı densin..
+         *
+         * Sonucu ortalama ile birlikte ekrana bassın.
+         *
+         * Örneğin : Ortalmanız 54.5  Büte girebilirsiniz...
+         */
 
-            /*
-            *Klavyeden 2 sayi girelim
-            *Önümüze bir menü çıksın
-            *
-            * Toplama için-> +
-            *Çıkarma için-> -
-            *Çarpma için-> *
-            *Bölme için-> /
-            *Birini seçiniz.....
-            *Seçtiği işlemin sonucunu yapıp ekrana yansıtsın.
-            */
-            Tekrar:
-            Console.WriteLine("Lütfen 2 sayi giriniz.");
-            double sayi1 = Convert.ToDouble(Console.ReadLine());
-            double sayi2 = Convert.ToDouble(Console.ReadLine());
+        Tekrar:
+            Console.WriteLine("Vize notunuzu giriniz: ");
+            int vize = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Lütfen yapacağınız işlemi seçiniz \n Toplama işlemi için : + \n Çıkarma işlemi için : - \n Çarpma işlemi için : * \n Bölme işlemi için : / \n");
-            string yapılacakIslem= Console.ReadLine();
-            double sonuc=0.0;
-            
+            Console.WriteLine("Final notunuzu giriniz: ");
+            int final = Convert.ToInt32(Console.ReadLine());
 
-            if (yapılacakIslem == "+" )
+            Console.WriteLine("Ödev notunuzu giriniz: ");
+            int odev = Convert.ToInt32(Console.ReadLine());
+
+            double ort;
+            ort = vize * 30.0 / 100 + final * 40.0 / 100 + odev * 30.0 / 100;
+
+            if (ort < 40)
             {
-                sonuc = sayi1 + sayi2;
+                Console.WriteLine("Dersten başarısız oldun seneye tekrar al.");
+
             }
 
-            if (yapılacakIslem == "-" )
+            else if (ort>40 && ort <60)
             {
-                sonuc = sayi1 - sayi2;
+                Console.WriteLine("Büte girebilirsiniz.");
             }
 
-            if (yapılacakIslem == "*")
+            else
             {
-                sonuc = sayi1 * sayi2;
+                Console.WriteLine("Dersi başarıyla geçtin.");
             }
-
-            if (yapılacakIslem == "/")
-            {
-                sonuc = sayi1 / sayi2;
-            }
-
-            Console.WriteLine( "Yaptığınız işlemin sonucu:" + sonuc);
-
 
             goto Tekrar;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
+
+
     }
 }
